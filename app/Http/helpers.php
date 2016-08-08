9a39d802144716b69e7b3339c442c4fb5e6a7e11
gergelypap <gpap@brainsum.com>
2016-08-08 14:23:03 +0200

    added comment for shitty elixir helper function

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,9 +1,18 @@
 <?php
 
-function elixirAsset($file) {
-	if (env('APP_ENV') === 'production') {
-		return elixir($file);
-	}
+if (! function_exists('elixirAsset')) {
+    /**
+     * Generate the correct path of given asset based on app environment.
+     *
+     * @param string $file
+     *
+     * @return string
+     */
+    function elixirAsset($file) {
+        if (env('APP_ENV') === 'production') {
+            return elixir($file);
+        }
 
-	return asset($file);
+        return asset($file);
+    }
 }
